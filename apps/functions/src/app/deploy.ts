8c67db287f045a5870476304ed5e59137886fc76
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-08-18 22:27:41 +0200

    Make ssr work for real

--- apps/functions/src/app/deploy.ts
+++ apps/functions/src/app/deploy.ts
@@ -1,12 +1,11 @@
 import * as functions from 'firebase-functions';
-import { join } from 'path';
 
 // __non_webpack_require__ ensure webpack uses "require" at runtime
 declare const __non_webpack_require__: any;
 const expressApp = __non_webpack_require__('./project-manhattan/server/main').app();
 
 export const ssr = functions
-  .region('europe-west2')
+  .region('us-central1') // ssr only work with us-central1 (https://firebase.google.com/docs/hosting/functions)
   .runWith({"timeoutSeconds":60,"memory":"1GB"})
   .https
   .onRequest(expressApp);
