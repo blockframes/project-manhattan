bdf10ab1e074f172584f6d0bd648f28a1275b850
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-08-18 17:46:47 +0200

    initial commit

---
+++ apps/project-manhattan/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-manhattan-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'project-manhattan';
+}
