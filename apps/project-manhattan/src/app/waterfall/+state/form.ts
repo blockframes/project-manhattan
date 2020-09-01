11ba14987b69e84e669a697d4f6049cd6afcbffb
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-09-01 11:44:41 +0200

    set multiplier to 1000 by default

--- apps/project-manhattan/src/app/waterfall/+state/form.ts
+++ apps/project-manhattan/src/app/waterfall/+state/form.ts
@@ -7,7 +7,7 @@ export function createForm(terms: Terms[]) {
     termsForm.addControl(term.id, new FormControl())
   }
   return new FormGroup({
-    multiplier: new FormControl(1),
+    multiplier: new FormControl(1000),
     name: new FormControl(),
     ticket: new FormGroup({
       amount: new FormControl(null, Validators.required),
