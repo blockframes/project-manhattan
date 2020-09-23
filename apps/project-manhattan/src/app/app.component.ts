033861f2e44acc176178c5e241c9c8c9627bb959
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-09-23 10:47:36 +0200

    update after review

--- apps/project-manhattan/src/app/app.component.ts
+++ apps/project-manhattan/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, HostBinding, ViewChild } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routeAnimation } from './animations';
 
@@ -8,7 +8,7 @@ import { routeAnimation } from './animations';
   <ng-container *transloco="let t">
     <mat-toolbar fxLayoutAlign="space-between center">
       <a class="img-link" routerLink="movie/list">
-        <img src="assets/img/upload.svg" />
+        <img src="assets/img/blockframes.png" />
       </a>
       <a mat-stroked-button routerLink="movie/create">
         <mat-icon>add</mat-icon>
@@ -23,7 +23,7 @@ import { routeAnimation } from './animations';
   styles: [
     ':host { display: block; height: 100%; }',
     'mat-toolbar { padding: 0 36px; }',
-    '.img-link { height: 80%; }',
+    '.img-link { height: 60%; }',
     'img { height: 100%; }',
     'main { height: calc(100% - 64px) }'
   ],
